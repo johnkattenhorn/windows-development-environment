9e09a5e65d435becfa87492b5521257e419ae6c2
John Kattenhorn <john.kattenhorn@applicita.com>
2018-07-20 23:24:29 +0100

    Updated Visual Studio 2017

--- development.ps1
+++ development.ps1
@@ -17,29 +17,17 @@ npm install -g npm-windows-upgrade
 #-- Angular Development ---
 #nvm install 9.5.0
 #nvm use 9.5.0
-#RefreshEnv.cmd
+#refreshenv
 # Install Angular CLI
 #npm install -g @angular/cli
 
-#-- Docker for Windows
-choco install -y docker-for-windows
-
 #--- Visual Studio 2017 ---
-choco install -y --allow-empty-checksums visualstudio2017enterprise
-choco install -y --allow-empty-checksums visualstudio2017-workload-azure
-choco install -y --allow-empty-checksums visualstudio2017-workload-data
-choco install -y --allow-empty-checksums visualstudio2017-workload-manageddesktop
-choco install -y --allow-empty-checksums visualstudio2017-workload-netcoretools
-choco install -y --allow-empty-checksums visualstudio2017-workload-netweb
-choco install -y --allow-empty-checksums visualstudio2017-workload-office
-choco install -y --allow-empty-checksums visualstudio2017-workload-managedgame
-choco install -y --allow-empty-checksums visualstudio2017-workload-nativecrossplat
-choco install -y --allow-empty-checksums visualstudio2017-workload-nativedesktop
-choco install -y --allow-empty-checksums visualstudio2017-workload-netcrossplat
+choco install -y visualstudio2017enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive" --execution-timeout=36000
 
-choco install -y --allow-empty-checksums visualstudio2017-workload-node
-choco install -y --allow-empty-checksums visualstudio2017-workload-universal
-choco install -y --allow-empty-checksums visualstudio2017-workload-webcrossplat
+#--- Docker ---
+choco install -y docker --execution-timeout=36000
+choco install -y docker-for-windows --execution-timeout=36000
+choco install -y docker-toolbox --params '-ia /TASKS="desktopicon,modifypath,upgradevm"' --execution-timeout=36000
 
 #--- JetBrains Resharper 2018.1 ---
 choco install -y resharper-platform
@@ -58,9 +46,6 @@ choco install -y sql-operations-studio
 
 #--- Microsoft SQL Server Data Tools ---
 choco install -y ssdt17
-
-#--- Service Fabric Tools ---
-
 
 if (Test-PendingReboot) { Invoke-Reboot }
 
